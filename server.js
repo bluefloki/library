bbb241d48d40abf5831fe5a495743967dc24c597
meerankazmi <kazmimeeran@gmail.com>
2020-03-13 10:10:27 +0500

    Initial Author Routes

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+const bodyParser = require("body-parser");
+
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 
 const indexRouter = require("./routes/index");
+const auhtorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.set(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
@@ -24,6 +28,7 @@ db.on("error", error => console.error(error));
 db.once("open", () => console.log("Connected to mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", auhtorRouter);
 
 app.listen(process.env.PORT || 3000, () =>
   console.log(`server started on port 3000`)
